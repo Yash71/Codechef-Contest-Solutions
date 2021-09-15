229d5ccaf5cd507b4aa82edd156ba1c7e2a33a29
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-09-16 05:29:21 +0530

    September Long Challenge

---
+++ September_Long_'21/Travel_Pass.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+        string s;
+        cin>>s;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0') ans+=a;
+            else ans+=b;
+        }        
+        cout<<ans<<"\n";
+
+    }
+}
