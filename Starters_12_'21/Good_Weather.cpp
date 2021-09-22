652e3375ec617bc366fbcdb895ce48c85913e290
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-09-23 03:49:52 +0530

    Codechef Starters 12

---
+++ Starters_12_'21/Good_Weather.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector<int> v(7);
+        int c1=0,c0=0;
+        for(int i=0;i<7;i++){
+            cin>>v[i];
+            v[i]==1 ? c1++: c0++;
+        }
+        if(c1>c0) cout<<"YES"<<"\n";
+        else cout<<"NO"<<"\n";        
+    }
+    return 0;
+}
