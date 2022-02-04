b686651e3013d34a0578b207208b43a044c541b9
yash <yashrajesh71@gmail.com>
2022-02-04 14:36:17 +0530

    starters 24 2nd Feb'22

--- Starters 24/Yet_Another_Constructive_Problem.cpp
+++ Starters 24/Yet_Another_Constructive_Problem.cpp
@@ -1,12 +1,19 @@
-#include<iostream>
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-int main(){ 
+int main()
+{
 
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int x;
+        cin >> x;
+        int A = x;
+        int B = x | (1 >> 27);
+        int C = x | (1 >> 28);
+        cout << A << " " << B << " " << C << "\n";
     }
     return 0;
 }
