02c0bea3fd2bbbaf8b173886d726e149292fd17d
yash <yashrajesh71@gmail.com>
2022-02-04 11:35:52 +0530

    Starters 24 2nd Feb'22

--- Starters 24/Anti_Palindrome.cpp
+++ Starters 24/Anti_Palindrome.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 bool palindrome(string str, int start, int end)
 {
-    bool flag = true;
-    while (start < end)
+    bool flag = false;
+    while (start <= end)
     {
-        if (str[start] != str[end])
+        if (str[start] == str[end])
         {
-            flag = false;
+            flag = true;
             break;
         }
         start++;
@@ -25,43 +25,16 @@ int main()
     cin >> t;
     while (t--)
     {
-        string s;
         int n;
         cin >> n;
+        string s;
         cin >> s;
-        if (n == 3 || n % 2 != 0)
-        {
-            cout << "NO";
-        }
-        else
-        {
-            // if(!palindrome(s,0,s.size()-1)){
-            int middle = (n - 1) / 2;
-            // cout<<middle;
-            if (palindrome(s, 0, middle) || palindrome(s, middle + 1, n - 1))
-            {
-                cout << "NO";
-            }
-            else
-            {
-                cout << "YES";
-                cout << "\n";
-
-                string subString1 = "";
-                string subString2 = "";
-                for (int i = 0; i <= middle; i++)
-                {
-                    subString1 += s[i];
-                }
-                for (int i = middle + 1; i < n; i++)
-                {
-                    subString2 += s[i];
-                }
-                reverse(subString1.begin(), subString1.end());
-                cout << subString1;
-                cout << subString2;
-            }
-        }
+        sort(s.begin(), s.end());
+        reverse(s.begin() + n / 2, s.end());
+        // cout<<s;
+        // cout<<palindrome(s,0,n-1);
+        palindrome(s, 0, n - 1) ? cout << "NO" : cout << "YES\n"
+                                                      << s;
         cout << "\n";
     }
     return 0;
