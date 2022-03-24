4ca867d9d3fb9e132e7c63276efb444b383cae2e
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2022-03-25 00:40:20 +0530

    Starters 31 23rd Mar '22

--- Starters 31/Sign_Moves.cpp
+++ Starters 31/Sign_Moves.cpp
@@ -5,8 +5,19 @@ int main(){
     int t;
     cin>>t;
     int res=0;
-    for(int i=0;i<t;i++){
-        int 
+    // for(int i=0;i<t;i++){
+    //     int steps=0;
+    //     cin>>steps;
+    //     if(res>=0)
+    // }
+    while(t--){
+        int n;
+        cin>>n;
+        // int ans= (n+1)/2;
+        // cout<< (n%2==0 ? ans : -ans) <<"\n";
+        cout<< (n%2!=0 ? -(n+1)/2 : n/2)<<"\n";
     }
     return 0;
+
+    // 011 & 111 => 011 
 }
